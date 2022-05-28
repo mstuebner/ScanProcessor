b736625508819e7de86d936197e240a2ed5d688a
MS <mstuebner@gmail.com>
2022-05-28 23:25:48 +0200

    Implement Timer that is reset when ever a new file is added, otherwise starts processing after TIMEOUT seconds

--- src/scan_processor/monitor_scanner_dir.py
+++ src/scan_processor/monitor_scanner_dir.py
@@ -4,6 +4,7 @@ Module implements a watchdog that monitors a directory for new files to process
 import logging
 import sys
 import time
+import threading
 import zc.lockfile
 
 # Third party modules
@@ -18,25 +19,60 @@ logging.basicConfig(level=logging.DEBUG,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 LOG = logging.getLogger('scan_processor.directory_monitor')
+LOG.setLevel(level=logging.INFO)
+
+TIMEOUT = 10
+TIMER_THREAD = None
+
+
+def process_files(filepath: str):
+    """
+    When this function is called:
+    - Colled all *.pdf files in the defined directory
+    - merge them into one pdf in the target directory
+    - delete original pdfs
+    """
+    LOG.info(f"{filepath}")
+    LOG.info('MERGE finished')
+
+
+def start_timer_thread(timer):
+    """
+    This function is called each time a new file is detected and starts or restarts the timer after that
+    pdfs are merged
+    """
+    if timer is not None:
+        timer.cancel()
+        LOG.debug('RESET timer')
+
+    timer = threading.Timer(TIMEOUT, process_files, [settings])
+    timer.start()
+    LOG.debug('START timer')
+
+    return timer
+
+
+def create_event_handler():
+    patterns = ["*.txt"]
+    ignore_patterns = None
+    ignore_directories = True
+    case_sensitive = True
+
+    return PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+
+def on_created(event):
+    """Event handler for CREATE event, fires when a new file is created"""
+    global TIMER_THREAD
+
+    LOG.info("The file \"%s\" has been created! Starting scan processor time", event.src_path)
+    TIMER_THREAD = start_timer_thread(timer=TIMER_THREAD)
 
 
 def monitoring(monitoring_settings):
     """Main function to monitor the directory defined in monitoring_settings.directory"""
-    patterns = ["*"]
-    ignore_patterns = None
-    ignore_directories = False
-    case_sensitive = True
-
-    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    my_event_handler = create_event_handler()
     LOG.debug('PatternMatchingEventHandler created')
-
-    def on_created(event):
-        """Event handler for CREATE event, fires when a new file is created"""
-        if not event.is_directory:
-            LOG.info("The file \"%s\" has been created!", event.src_path)
-            LOG.info("Starting scan processor")
-
-            LOG.info("Scan processor importer ended\n-------------------------")
 
     my_event_handler.on_created = on_created
     LOG.debug('"on_created" eventhandler added')
@@ -59,7 +95,7 @@ if __name__ == "__main__":
     LOCK = None
 
     try:
-        LOCK = zc.lockfile.LockFile(APPLICATION)
+        LOCK = zc.lockfile.LockFile(f'{APPLICATION}.lock')
         LOG.info('%s started.', APPLICATION)
         monitoring(monitoring_settings=settings)
         LOCK.close()
