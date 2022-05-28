ebcbff07d07d594e398995174575462fb0a57042
MS <mstuebner@gmail.com>
2022-05-29 01:16:29 +0200

    Fully functional code, refactoring to do

--- src/scan_processor/monitor_scanner_dir.py
+++ src/scan_processor/monitor_scanner_dir.py
@@ -2,9 +2,12 @@
 Module implements a watchdog that monitors a directory for new files to process
 """
 import logging
+import os
 import sys
 import time
 import threading
+import typing
+
 import zc.lockfile
 
 # Third party modules
@@ -13,6 +16,7 @@ from watchdog.observers import Observer
 
 # Project owned modules
 from config_model import settings
+import pdf_merger
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(message)s',
@@ -21,18 +25,33 @@ logging.basicConfig(level=logging.DEBUG,
 LOG = logging.getLogger('scan_processor.directory_monitor')
 LOG.setLevel(level=logging.INFO)
 
-TIMEOUT = 10
 TIMER_THREAD = None
 
 
-def process_files(filepath: str):
+def collect_pdfs_to_merge(scan_directory: str) -> typing.List[str]:
+    """
+    Function scans the directory and returns a list of absolut filepathes
+    """
+    pdf_list = []
+
+    if os.path.exists(scan_directory):
+        file_list = os.listdir(path=scan_directory)
+        pdf_list = [os.path.join(scan_directory, item) for item in file_list]
+
+    return pdf_list
+
+
+def process_files():
     """
     When this function is called:
-    - Colled all *.pdf files in the defined directory
-    - merge them into one pdf in the target directory
-    - delete original pdfs
+    - Collect all *.pdf files in the defined scan directory: Done
+    - merge them into one pdf in the target directory: Done
+    - delete original pdfs: Done
     """
-    LOG.info(f"{filepath}")
+    pdf_list = collect_pdfs_to_merge(scan_directory=settings.scan_directory)
+    pdf_list = [f.strip() for f in pdf_list if f.endswith('.pdf')]
+
+    pdf_merger.merge_pdfs(list_of_pdfs=pdf_list, output_path=settings.output_path)
     LOG.info('MERGE finished')
 
 
@@ -45,15 +64,18 @@ def start_timer_thread(timer):
         timer.cancel()
         LOG.debug('RESET timer')
 
-    timer = threading.Timer(TIMEOUT, process_files, [settings])
+    timer = threading.Timer(settings.timeout, process_files)
     timer.start()
-    LOG.debug('START timer')
+    LOG.debug('START timer: %s', str(timer))
 
     return timer
 
 
-def create_event_handler():
-    patterns = ["*.txt"]
+def create_event_handler() -> PatternMatchingEventHandler:
+    """
+    Function creates and returns an event handle for the observer to monitor the file system
+    """
+    patterns = settings.filepattern
     ignore_patterns = None
     ignore_directories = True
     case_sensitive = True
@@ -62,11 +84,26 @@ def create_event_handler():
 
 
 def on_created(event):
-    """Event handler for CREATE event, fires when a new file is created"""
+    """
+    Event handler for CREATE event, fires when a new file is created
+    """
     global TIMER_THREAD
 
-    LOG.info("The file \"%s\" has been created! Starting scan processor time", event.src_path)
-    TIMER_THREAD = start_timer_thread(timer=TIMER_THREAD)
+    if file_in_scandir(filepath=os.path.split(event.src_path)[0], scan_dir=settings.scan_directory):
+        LOG.info("The file \"%s\" has been created! Starting scan processor TIMER", event.src_path)
+        TIMER_THREAD = start_timer_thread(timer=TIMER_THREAD)
+    else:
+        LOG.debug("The file \"%s\" is not in the scan dir itself. Not processing it.", event.src_path)
+
+
+def file_in_scandir(filepath: str, scan_dir: str) -> bool:
+    """
+    Function compares filepath (the path of the event) and scan_dir (the directory to scan for files) and returns
+    True is they are identical. This test is required, because the watchdog module is monitoring directories
+    recursively, not only the given directory. The recursive scan would lead to an endless loop, in case merged
+    files are created ina subdirectory of scan_dir.
+    """
+    return filepath == scan_dir
 
 
 def monitoring(monitoring_settings):
@@ -78,7 +115,7 @@ def monitoring(monitoring_settings):
     LOG.debug('"on_created" eventhandler added')
 
     observer = Observer()
-    observer.schedule(my_event_handler, monitoring_settings.directory, recursive=True)
+    observer.schedule(my_event_handler, monitoring_settings.scan_directory, recursive=True)
     observer.start()
     LOG.debug('Observer scheduled and started.')
     try:
